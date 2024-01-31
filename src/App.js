133d3b3cdbdcacf27e9b5a6ca188e6bf0b8d0501
Abrham Desalegn <119532591+abrham-desalegn@users.noreply.github.com>
2024-01-31 21:51:30 +0300

    Third commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,6 @@
-import React from 'react'
-import Header from './components/Header/Header';
-import Footer from './components/Footer/Footer';
 
 function App() {
-  return (
-    <div>
-      <Header />
-      <Footer />
-    </div>
-  );
+  return <h1>Test</h1>;
 }
 
 export default App;
